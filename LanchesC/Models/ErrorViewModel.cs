9776389a6ff06f847d25e0a7451eb3e541ac6a96
Douglas Samuel <douglassam007@gmail.com>
2022-01-07 09:14:04 -0300

    Add project files.

---
+++ LanchesC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LanchesC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ LanchesC/Models/Snack.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanchesC.Models
+{
+    public class Snack
+    {
+        public int SnackId { get; set; }
+        public string Name { get; set; }
+        public string ShotDescription { get; set; }
+        public string DetailedDescription { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageThumbUrl { get; set; }
+        public bool IsPreferredSnack { get; set; }
+        public bool InStock { get; set; }
+    }
+}
