399f6a3898477338654ab0eff5ce3e8d1f7e9748
Douglas Samuel <douglassam007@gmail.com>
2022-01-11 10:59:43 -0300

    Adding Cart view model.

---
+++ LanchesC/Models/ViewModels/CartViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanchesC.Models.ViewModels
+{
+    public class CartViewModel
+    {
+        public Cart Cart { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
