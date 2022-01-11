3ed91aefd3f7e2eabf9423b3a52b0eb99432fe62
Douglas Samuel <douglassam007@gmail.com>
2022-01-11 12:10:58 -0300

    Adding images and cart resume component.

---
+++ LanchesC/Components/CartResume.cs
@@ -0,0 +1,31 @@
+﻿using LanchesC.Models;
+using LanchesC.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesC.Components
+{
+    public class CartResume : ViewComponent
+    {
+        private readonly Cart _cart;
+
+        public CartResume(Cart cart)
+        {
+            _cart = cart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var itens = _cart.GetCartItems();
+
+            _cart.CartItems = itens;
+
+            var carVM = new CartViewModel
+            {
+                Cart = _cart,
+                CartTotal = _cart.GetCartTotal()
+            };
+
+            return View(carVM);
+        }
+    }
+}
