06b02da54a6e652faf42bd97be7be938b4c1b4e7
Douglas Samuel <douglassam007@gmail.com>
2022-01-11 14:54:17 -0300

    Adding Order repository to access database.

---
+++ LanchesC/Repositories/OrderRepository.cs
@@ -0,0 +1,44 @@
+﻿using LanchesC.Data;
+using LanchesC.Models;
+using LanchesC.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanchesC.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly Cart _cart;
+
+        public OrderRepository(AppDbContext appDbContext, Cart cart)
+        {
+            _appDbContext = appDbContext;
+            _cart = cart;
+        }
+
+        public void CreateOrder(Order order)
+        {
+            order.DateOrder = DateTime.Now;
+            _appDbContext.Orders.Add(order);
+            _appDbContext.SaveChanges();
+
+            var cartItens = _cart.CartItems;
+
+            foreach(var cartItem in cartItens)
+            {
+                var orderDetail = new OrderDetail()
+                {
+                    Quantity = cartItem.Quantity,
+                    SnackId = cartItem.Snack.SnackId,
+                    OrderId = order.OrderId,
+                    Price = cartItem.Snack.Price
+                };
+                _appDbContext.OrderDetails.Add(orderDetail);
+            }
+            _appDbContext.SaveChanges();
+        }
+    }
+}
--- LanchesC/Startup.cs
+++ LanchesC/Startup.cs
@@ -30,6 +30,7 @@ namespace LanchesC
 
             services.AddTransient<ISnackRepository, SnackRepository>();
             services.AddTransient<ICategoryRepository, CategoryRepository>();
+            services.AddTransient<IOrderRepository, OrderRepository>();
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped(sp => Cart.GetCart(sp));
 
