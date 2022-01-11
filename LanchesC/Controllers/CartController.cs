a5f9223bf6f6551e2805bade9076e193bdc688e9
Douglas Samuel <douglassam007@gmail.com>
2022-01-11 11:16:36 -0300

    Adding cart controller and index view

---
+++ LanchesC/Controllers/CartController.cs
@@ -0,0 +1,60 @@
+﻿using LanchesC.Models;
+using LanchesC.Models.ViewModels;
+using LanchesC.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanchesC.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ISnackRepository _snackRepository;
+        private readonly Cart _cart;
+
+        public CartController(ISnackRepository snackRepository, Cart cart)
+        {
+            _snackRepository = snackRepository;
+            _cart = cart;
+        }
+
+        public IActionResult Index()
+        {
+            var itens = _cart.GetCartItems();
+            _cart.CartItems = itens;
+
+            var cartViewModel = new CartViewModel
+            {
+                Cart = _cart,
+                CartTotal = _cart.GetCartTotal()
+            };
+
+            return View(cartViewModel);
+        }
+        public IActionResult AddItemToCart(int snackId)
+        {
+            var selectedSnack = _snackRepository.Snacks
+                                    .FirstOrDefault(p => p.SnackId == snackId);
+
+            if (selectedSnack != null)
+            {
+                _cart.AddItemToCart(selectedSnack);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult RemoveFromCart(int snackId)
+        {
+            var selectedSnack = _snackRepository.Snacks
+                                    .FirstOrDefault(p => p.SnackId == snackId);
+
+            if (selectedSnack != null)
+            {
+                _cart.RemoveFromCart(selectedSnack);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
